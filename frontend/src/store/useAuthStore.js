06a2879f354903fbe0779f49bb290d8318b4ef7c
JustinPhan <hoangphan030103@gmail.com>
2026-01-02 16:13:36 -0500

    login page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -7,6 +7,7 @@ export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
+  isLoggingIn: false,
 
   // function to check if user is authenticated
   checkAuth: async () => {
@@ -34,5 +35,32 @@ export const useAuthStore = create((set) => ({
     } finally {
       set({ isSigningUp: false });
     }
-  }
+  },
+
+  // function to handle user login
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+
+      toast.success("Logged in successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  // function to handle user logout
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error("Error logging out");
+      console.log("Logout error:", error);
+    }
+  },
 }));
